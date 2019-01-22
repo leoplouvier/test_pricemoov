f79808f50bc1cd021acc15d012a528c2e9665035
leoplouvier <plouvierleo@gmail.com>
2019-01-22 14:51:06 +0100

    refacto fetching

--- src/Components/App.js
+++ src/Components/App.js
@@ -12,53 +12,31 @@ class App extends Component {
     prices: []
   };
 
-  handleChange = event => {
+  fetchData = async url => {
+    var response = await fetch(url)
+    var json = await response.json()
+    var data =JSON.parse(JSON.stringify(json))
+    return data
+  }
+
+  handleChange = async event => {
     this.setState({ [event.target.name]: event.target.value });
     if (event.target.name === "agencie") {
       var id = this.state.agencies.find(a => a.name === event.target.value).id;
-      fetch(
-        "http://5ae97684531a58001414278c.mockapi.io/agencies/" +
-          id +
-          "/categories"
-      )
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          this.setState({
-            categories: JSON.parse(JSON.stringify(data)),
-            disableCat: false
-          });
-        });
+      var categories = await this.fetchData("http://5ae97684531a58001414278c.mockapi.io/agencies/"+id+"/categories")
+      this.setState({ categories: categories, disableCat:false });
     }
     if (event.target.name === "categorie") {
       var aid = this.state.agencies.find(a => a.name === this.state.agencie).id;
-      var cid = this.state.categories.find(c => c.name === event.target.value)
-        .id;
-      fetch(
-        "http://5ae97684531a58001414278c.mockapi.io/agencies/" +
-          aid +
-          "/categories/" +
-          cid +
-          "/prices"
-      )
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          this.setState({ prices: JSON.parse(JSON.stringify(data)) });
-        });
+      var cid = this.state.categories.find(c => c.name === event.target.value).id;
+        var prices = await this.fetchData("http://5ae97684531a58001414278c.mockapi.io/agencies/"+aid+"/categories/"+cid+"/prices")
+        this.setState({ prices: prices });
     }
   };
 
-  componentDidMount = () => {
-    fetch("http://5ae97684531a58001414278c.mockapi.io/agencies")
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        this.setState({ agencies: JSON.parse(JSON.stringify(data)) });
-      });
+  componentDidMount = async () => {
+    var agencies = await this.fetchData("http://5ae97684531a58001414278c.mockapi.io/agencies")
+        this.setState({ agencies: agencies });
   };
 
   render() {
